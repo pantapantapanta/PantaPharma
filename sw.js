f6543dbbc48542b2edc732f566c448e45fc5d32a
Panta <leo.corvino@gmail.com>
2026-03-29 17:13:06 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'pharma-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'pharma-v5';
+const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png', '/apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
